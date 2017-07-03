8d7bb7d0c9f9748a336a0a1f35d81b1c587eb79f
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-07-03 13:09:29 +0200

    Updates tests

--- test/lib/generate-grunnlag-liste-test.js
+++ test/lib/generate-grunnlag-liste-test.js
@@ -2,18 +2,18 @@
 
 const tap = require('tap')
 const generateGrunnlagListe = require('../../lib/generate-grunnlag-liste')
-const test3770Folkeregistrert = require('../data/folkeregistrert-3770-test.json')
-const test3770Delt = require('../data/delt-3770-test.json')
-const test3770Hybel = require('../data/hybel-3770-test.json')
+const test3781Folkeregistrert = require('../data/folkeregistrert-3781-test.json')
+const test3781Delt = require('../data/delt-3781-test.json')
+const test3781Hybel = require('../data/hybel-3781-test.json')
 const test3681Folkeregistrert = require('../data/folkeregistrert-3681-test.json')
 const test3681Delt = require('../data/delt-3681-test.json')
 const test3681Hybel = require('../data/hybel-3681-test.json')
 
-tap.equal(3, generateGrunnlagListe(test3770Folkeregistrert).length, 'It returns expected result for 3770 folkeregistrert')
+tap.equal(3, generateGrunnlagListe(test3781Folkeregistrert).length, 'It returns expected result for 3781 folkeregistrert')
 
-tap.equal(3, generateGrunnlagListe(test3770Delt).length, 'It returns expected result for 3770 delt')
+tap.equal(3, generateGrunnlagListe(test3781Delt).length, 'It returns expected result for 3781 delt')
 
-tap.equal(3, generateGrunnlagListe(test3770Hybel).length, 'It returns expected result for 3770 hybel')
+tap.equal(3, generateGrunnlagListe(test3781Hybel).length, 'It returns expected result for 3781 hybel')
 
 tap.equal(2, generateGrunnlagListe(test3681Folkeregistrert).length, 'It returns expected result for 3681 folkeregistrert')
 

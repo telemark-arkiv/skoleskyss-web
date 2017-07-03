dfd25c98147c23af303083c8db87d875f279512f
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-07-03 14:15:25 +0200

    Removes data

--- views/helpers/showBosted.js
+++ views/helpers/showBosted.js
@@ -9,8 +9,8 @@ module.exports = data => {
     outputs.push('Hjemme, på folkeregistrert bosted')
     outputs.push(showDsfBosted(data.dsfData))
     if (data['distance-see-dsf']) {
-      outputs.push('Foreløpig beregnet avstand til skole: ' + data['distance-see-dsf'].data.distance)
-      outputs.push('<a href="' + data['distance-see-dsf'].data.staticMapUrl + '" target="_blank">Se beregnet rute</a>')
+      outputs.push('Foreløpig beregnet avstand til skole: ' + data['distance-see-dsf'].distance)
+      outputs.push('<a href="' + data['distance-see-dsf'].staticMapUrl + '" target="_blank">Se beregnet rute</a>')
     }
   }
 
@@ -19,15 +19,15 @@ module.exports = data => {
     outputs.push('Folkeregistrert bosted')
     outputs.push(showDsfBosted(data.dsfData))
     if (data['distance-see-dsf']) {
-      outputs.push('Foreløpig beregnet avstand til skole: ' + data['distance-see-dsf'].data.distance)
-      outputs.push('<a href="' + data['distance-see-dsf'].data.staticMapUrl + '" target="_blank">Se beregnet rute</a>')
+      outputs.push('Foreløpig beregnet avstand til skole: ' + data['distance-see-dsf'].distance)
+      outputs.push('<a href="' + data['distance-see-dsf'].staticMapUrl + '" target="_blank">Se beregnet rute</a>')
     }
     outputs.push('')
     outputs.push('Bosted nummer to')
     outputs.push(showDsfBosted(data.bosteddelt))
     if (data['distance-see-delt']) {
-      outputs.push('Foreløpig beregnet avstand til skole: ' + data['distance-see-delt'].data.distance)
-      outputs.push('<a href="' + data['distance-see-delt'].data.staticMapUrl + '" target="_blank">Se beregnet rute</a>')
+      outputs.push('Foreløpig beregnet avstand til skole: ' + data['distance-see-delt'].distance)
+      outputs.push('<a href="' + data['distance-see-delt'].staticMapUrl + '" target="_blank">Se beregnet rute</a>')
     }
   }
 
@@ -35,8 +35,8 @@ module.exports = data => {
     outputs.push('Hybel')
     outputs.push(showDsfBosted(data.bostedhybel))
     if (data['distance-see-hybel']) {
-      outputs.push('Foreløpig beregnet avstand til skole: ' + data['distance-see-hybel'].data.distance)
-      outputs.push('<a href="' + data['distance-see-hybel'].data.staticMapUrl + '" target="_blank">Se beregnet rute</a>')
+      outputs.push('Foreløpig beregnet avstand til skole: ' + data['distance-see-hybel'].distance)
+      outputs.push('<a href="' + data['distance-see-hybel'].staticMapUrl + '" target="_blank">Se beregnet rute</a>')
     }
   }
   return outputs.join('<br />')
